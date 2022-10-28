8daa630429cf87ea18472944442cece8cf470277
chand1012 <chandler@multisiglabs.org>
2022-10-28 17:00:16 -0400

    Update settings

--- movies/settings.py
+++ movies/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': os.environ.get("POSTGRES_HOST", "db"),
+        'PORT': os.environ.get("POSTGRES_PORT", 5432),
     }
 }
 
